4e36390cd65ce7809c46cd91322c5cfd5ef5e9f7
Jake Grella <jgrella13@gmail.com>
2020-10-15 15:54:59 -0500

    completed styling

--- user-onboarding/src/components/TeamMemberForm.js
+++ user-onboarding/src/components/TeamMemberForm.js
@@ -16,143 +16,149 @@ export default function Form(props) {
 
 	return (
 		<form onSubmit={onSubmit}>
-			<div className='form-section general-info'>
-				<h2>General Info</h2>
-				{/* Name */}
-				<label>
-					Name:
-					<input
-						value={values.name}
-						onChange={onChange}
-						name='name'
-						type='text'
-					/>
-				</label>
+			<div className='form-top'>
+				<div className='form-section general-info'>
+					<h2>General Info</h2>
+					{/* Name */}
+					<label>
+						Name:
+						<input
+							value={values.name}
+							onChange={onChange}
+							name='name'
+							type='text'
+						/>
+					</label>
 
-				{/* Username */}
-				<label>
-					Username:
-					<input
-						value={values.username}
-						onChange={onChange}
-						name='username'
-						type='text'
-					/>
-				</label>
+					{/* Username */}
+					<label>
+						Username:
+						<input
+							value={values.username}
+							onChange={onChange}
+							name='username'
+							type='text'
+						/>
+					</label>
 
-				{/* Email */}
-				<label>
-					Email:
-					<input
-						value={values.email}
-						onChange={onChange}
-						name='email'
-						type='email'
-					/>
-				</label>
+					{/* Email */}
+					<label>
+						Email:
+						<input
+							value={values.email}
+							onChange={onChange}
+							name='email'
+							type='email'
+						/>
+					</label>
 
-				{/* Password */}
-				<label>
-					Password:
-					<input
-						value={values.password}
-						onChange={onChange}
-						name='password'
-						type='password'
-					/>
-				</label>
+					{/* Password */}
+					<label>
+						Password:
+						<input
+							value={values.password}
+							onChange={onChange}
+							name='password'
+							type='password'
+						/>
+					</label>
+				</div>
+
+				<div className='form-section department-info'>
+					<h2>Department Info</h2>
+					{/* Department */}
+					<label>
+						Department:
+						<select
+							onChange={onChange}
+							value={values.department}
+							name='department'
+						>
+							<option value=''>--Select a Department--</option>
+							<option value='engineering'>Engineering</option>
+							<option value='marketing'>Marketing</option>
+							<option value='finance'>Finance</option>
+						</select>
+					</label>
+
+					{/* Team */}
+					<label>
+						Team:
+						<select onChange={onChange} value={values.team} name='team'>
+							<option value=''>--Select a Team--</option>
+							<option value='ios'>iOS</option>
+							<option value='web'>Web</option>
+							<option value='international-sales'>International Sales</option>
+							<option value='influencer-partnerships'>
+								Influencer Partnerships
+							</option>
+							<option value='venture-capital'>Venture Capital</option>
+						</select>
+					</label>
+
+					{/* Supervisor */}
+					<label>
+						Supervisor:
+						<input
+							value={values.supervisor}
+							onChange={onChange}
+							name='supervisor'
+							type='text'
+						/>
+					</label>
+
+					{/* Location */}
+					<label>
+						Location:
+						<select onChange={onChange} value={values.location} name='location'>
+							<option value=''>--Select a Location--</option>
+							<option value='los-angeles'>Los Angeles, CA</option>
+							<option value='austin'>Austin, TX</option>
+							<option value='chicago'>Chicago, IL</option>
+							<option value='boston'>Boston, MA</option>
+							<option value='london'>London, U.K.</option>
+						</select>
+					</label>
+				</div>
 			</div>
 
-			<div className='form-section department-info'>
-				<h2>Department Info</h2>
-				{/* Department */}
-				<label>
-					Department:
-					<select
-						onChange={onChange}
-						value={values.department}
-						name='department'
-					>
-						<option value=''>--Select a Department--</option>
-						<option value='engineering'>Engineering</option>
-						<option value='marketing'>Marketing</option>
-						<option value='finance'>Finance</option>
-					</select>
-				</label>
+			<div className='form-bottom'>
+				<div className='form-section agreements-checkbooxes submit'>
+					{/* Terms of Service */}
+					<label>
+						<input
+							type='checkbox'
+							name='terms'
+							checked={values.terms}
+							onChange={onChange}
+							required
+						/>
+						Please agree to our Terms of Service.
+					</label>
 
-				{/* Team */}
-				<label>
-					Team:
-					<select onChange={onChange} value={values.team} name='team'>
-						<option value=''>--Select a Team--</option>
-						<option value='ios'>iOS</option>
-						<option value='web'>Web</option>
-						<option value='international-sales'>International Sales</option>
-						<option value='influencer-partnerships'>
-							Influencer Partnerships
-						</option>
-						<option value='venture-capital'>Venture Capital</option>
-					</select>
-				</label>
-
-				{/* Supervisor */}
-				<label>
-					Supervisor:
-					<input
-						value={values.supervisor}
-						onChange={onChange}
-						name='supervisor'
-						type='text'
-					/>
-				</label>
-
-				{/* Location */}
-				<label>
-					Location:
-					<select onChange={onChange} value={values.location} name='location'>
-						<option value=''>--Select a Location--</option>
-						<option value='los-angeles'>Los Angeles, CA</option>
-						<option value='austin'>Austin, TX</option>
-						<option value='chicago'>Chicago, IL</option>
-						<option value='boston'>Boston, MA</option>
-						<option value='london'>London, U.K.</option>
-					</select>
-				</label>
-			</div>
-
-			<div className='form-section agreements-checkbooxes submit'>
-				{/* Terms of Service */}
-				<label>
-					<input
-						type='checkbox'
-						name='terms'
-						checked={values.terms}
-						onChange={onChange}
-					/>
-					Please agree to our Terms of Service.
-				</label>
-
-				{/* Privacy Policy */}
-				<label>
-					<input
-						type='checkbox'
-						name='privacy'
-						checked={values.privacy}
-						onChange={onChange}
-					/>
-					Please agree to our Privacy Policy.
-				</label>
-				{/* Privacy Policy */}
-				<label>
-					<input
-						type='checkbox'
-						name='gum'
-						checked={values.gum}
-						onChange={onChange}
-					/>
-					Check this box if you agree to work on the weekends with Trident
-					Layers as a form of payment.
-				</label>
+					{/* Privacy Policy */}
+					<label>
+						<input
+							type='checkbox'
+							name='privacy'
+							checked={values.privacy}
+							onChange={onChange}
+							required
+						/>
+						Please agree to our Privacy Policy.
+					</label>
+					{/* Trident */}
+					<label>
+						<input
+							type='checkbox'
+							name='gum'
+							checked={values.gum}
+							onChange={onChange}
+						/>
+						Check this box if you agree to work on the weekends with Trident
+						Layers as a form of payment.
+					</label>
+				</div>
 
 				{/* validation errors */}
 				<div className='errors'>
